8186aaf65cbd5efba51b353c95979e410201cb46
michals-src <c_ukiereczek.p@onet.pl>
2020-10-07 21:56:25 +0200

    Podstawowa struktura elementu label dotted

---
+++ src/dotted-labels/edit.js
@@ -0,0 +1,24 @@
+
+import './edit.css'
+
+function edit3({
+    attributes,
+    setAttributes
+}) {
+    return (
+        <>
+        <div className="selleads-label--object">
+            <div className="selleads-label--item">
+                <div className="selleads-label-dot"><span>Dot</span></div>
+                <div className="selleads-label-container">
+                    <h5 className="selleads-label--header">Hello world</h5>
+                    <p className="selleads-label--content">Ala ma kota, a kot ma alę</p>
+                    <p className="selleads-label--content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default edit3
