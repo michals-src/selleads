73a10ce0fc473c98b3fdb5403c0a50c1107f0e6d
michals-src <c_ukiereczek.p@onet.pl>
2020-10-14 00:16:57 +0200

    Dodawanie kolumn

--- src/columns/edit.js
+++ src/columns/edit.js
@@ -1,3 +1,45 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import { dropRight, get, map, times } from 'lodash';
+
+import colors from '.././colors.js';
+import { plus, column, columns } from '@wordpress/icons'
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { 
+	PanelBody, RangeControl, Notice, Placeholder, 
+	Toolbar, ToolbarButton, ToolbarGroup, Button,
+	ColorPalette, SelectControl, 
+	__experimentalNumberControl as NumberControl,
+	Tooltip
+} from '@wordpress/components';
+
+import {
+	InspectorControls,
+	InnerBlocks,
+	BlockControls,
+	BlockIcon,
+	BlockVerticalAlignmentToolbar,
+	__experimentalBlockVariationPicker,
+	__experimentalUseBlockWrapperProps as useBlockWrapperProps,
+} from '@wordpress/block-editor';
+import { withDispatch, useDispatch, useSelect } from '@wordpress/data';
+import { useState, Fragment, useEffect, useRef, } from '@wordpress/element';
+import { createBlock } from '@wordpress/blocks';
+
+import { withFilters } from '@wordpress/components';
+import { addFilter } from '@wordpress/hooks';
+import { compose, withInstanceId, useInstanceId, createHigherOrderComponent} from '@wordpress/compose';
+
+import { sumBy, merge, mapValues } from 'lodash';
+
+import './edit.css';
+
+
 
 const ALLOWED_BLOCKS = [ 'core/column' ];
 const INNER_BLOCKS_TEMPLATE = [
@@ -10,13 +52,14 @@ const INNER_BLOCKS_TEMPLATE = [
 const ColumnToolBox = ({
 	isSelected,
 	isHovering,
+	onClick,
 	...props
 }) => {
 
 	return (
 		<div>
 
-			{ ( isSelected || isHover ) && (
+			{ ( isSelected || isHovering ) && (
 				<div style={{
 					textAlign: "center",
 					padding: "15px"
@@ -24,7 +67,7 @@ const ColumnToolBox = ({
 				
 					<Toolbar>
 						<Tooltip text="Dodaj nowy wiersz">
-							<ToolbarButton>
+							<ToolbarButton onClick={onClick}>
 									<BlockIcon icon={ columns } />
 							</ToolbarButton>
 						</Tooltip>
@@ -37,8 +80,76 @@ const ColumnToolBox = ({
 	)
 }
 
+const createBlocksFromInnerBlocksTemplate = ( innerBlocksTemplate ) => {
+	return map(
+		innerBlocksTemplate,
+		( [ name, attributes, innerBlocks = [] ] ) =>
+			createBlock(
+				name,
+				attributes,
+				createBlocksFromInnerBlocksTemplate( innerBlocks )
+			)
+	);
+};
 
 function edit(props) {
+
+	const {
+		clientId,
+		attributes,
+		setAttributes,
+		updateColumns
+	} = props;
+
+	const { 
+		backgroundColor, 
+		borderRadius, 
+		padding,
+	} = attributes;
+
+
+	const { getInnerBlocks, hasInnerBlocks } = useSelect( (select) => {
+		return {
+			getInnerBlocks: select('core/block-editor').getBlocks( clientId ),
+			hasInnerBlocks: select('core/block-editor').getBlocks( clientId ).length > 0,
+		};
+	}, [clientId]);
+
+
+	const { replaceInnerBlocks } = useDispatch( 'core/block-editor' );
+	const [ columns, setColumns ] = useState(1);
+	const [ type, setType ] = useState('selleads/column');
+
+	const onUpdateColumnsHandler = (e) => {
+		console.log(e);
+	};
+
+	const { updateBlockAttributes } = useDispatch( 'core/block-editor' );
+	
+	const [ index, setIndex ] = useState(1);
+
+	useEffect(() => {
+		getInnerBlocks.map( (e) => {
+			console.log(e.innerBlocks.length);
+		});
+	}, [getInnerBlocks]);
+
+	const onItemsCreate = () => {
+
+
+		updateColumns(1,2);
+		//console.log(getBlocks( clientId ));
+
+		// replaceInnerBlocks(
+		// 	clientId,
+		// 	createBlocksFromInnerBlocksTemplate(
+		// 		[[ 'selleads/row', {
+		// 			onUpdateColumns: onUpdateColumns
+		// 		} ]]
+		// 	)
+		// );
+	};
+
 	return (
 		<>
 		<InspectorControls>
@@ -73,24 +184,31 @@ function edit(props) {
 
 		<div className="columns-parent" 
 			style={{
-				padding: `${padding}px`,
+				padding: `30px`,
 				backgroundColor: backgroundColor,
 				borderRadius: `${borderRadius}px`
 			}}>
 				<div className="columns-container">
 
-					<InnerBlocks
-						templateLock="all"
-						allowedBlocks={ ALLOWED_BLOCKS }
-						template={ INNER_BLOCKS_TEMPLATE }
-						orientation="horizontal"
-						__experimentalPassedProps={ {
-							className: 'columns-inner-block'
-						} }
-						renderAppender={ false }
-					/>
+				{
+					hasInnerBlocks ? (
+						<InnerBlocks
+							templateLock="all"
+							allowedBlocks={ ALLOWED_BLOCKS }
+							renderAppender={ false }
+						/>
+					) : (
+						<InnerBlocks
+							templateLock="all"
+							allowedBlocks={ ALLOWED_BLOCKS }
+							template={ INNER_BLOCKS_TEMPLATE }
+							orientation="horizontal"
+							renderAppender={ false }
+						/>
+					)
+				}
 
-					<ColumnToolBox {...props} />
+					<ColumnToolBox onClick={() => onItemsCreate() } {...props} />
 
 				</div>
 		</div>
@@ -98,4 +216,26 @@ function edit(props) {
 	)
 }
 
-export default edit
+export default withDispatch(
+	( dispatch, ownProps, registry ) => ( {
+
+		updateColumns( previousColumns, newColumns ) {
+			const { clientId } = ownProps;
+			const { replaceInnerBlocks } = dispatch( 'core/block-editor' );
+			const { getBlocks } = registry.select( 'core/block-editor' );
+
+			let innerBlocks = getBlocks( clientId );
+			
+			// Redistribute available width for existing inner blocks.
+			const isAddingColumn = newColumns > previousColumns;
+
+			if ( isAddingColumn ) {
+				let a = createBlock( 'selleads/row' );
+				innerBlocks = [
+					...innerBlocks, a
+				];
+			}
+			replaceInnerBlocks( clientId, innerBlocks, false );
+		},
+	} )
+)( edit );
