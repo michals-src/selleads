df1935522865967ef136e41c360acda6d35b9728
michals-src <c_ukiereczek.p@onet.pl>
2020-10-13 13:08:43 +0200

    Eksport domyślnych kolorów

---
+++ src/colors.js
@@ -0,0 +1,8 @@
+const colors = [
+    { name: 'red', color: '#efede5' },
+    { name: 'white', color: '#bfb97d' },
+    { name: 'blue', color: '#84bf7d' },
+    { name: 'white blue', color: '#afd5d4' },
+];
+
+export default colors;
